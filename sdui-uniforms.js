b9aa2b0c822f9ddc19eb41c9c69a2321feb63f8c
JanMP <pille@mac.com>
2021-07-12 15:20:47 +0200

    handover

--- sdui-uniforms.js
+++ sdui-uniforms.js
@@ -6,6 +6,7 @@ export const name = 'sdui-uniforms';
 export { default as AutoForm } from './imports/uniforms-custom/AutoForm.tsx';
 export { default as AutoField } from './imports/uniforms-custom/AutoField';
 export { default as DynamicField } from './imports/DynamicField.coffee';
+export { default as SubmitField } from './imports/uniforms-custom/SubmitField';
 export { default as FormModal } from './imports/FormModal';
 export { default as ConfirmationModal } from './imports/ConfirmationModal';
 export { default as meteorApply } from './imports/meteorApply'
